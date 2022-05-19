9d2177768d9b04a1dcd5adcf5b2b7c714196efb7
Saleem-Ur-Rehman <78738043+RealSaleem@users.noreply.github.com>
2022-05-20 01:39:34 +0500

    update

--- src/views/bookings/Seats.vue
+++ src/views/bookings/Seats.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-md-12">
           <div class="haz-exp">
-          <div class="row">
+            <div class="row">
               <div class="col-md-5">
                 <div class="dn">
                   <img
@@ -13,9 +13,15 @@
                   />
                   <h4>
                     {{ selectedTrain.trainName }} (
-                    {{ selectedTrain.trainCode }} )  <v-btn rounded color="info" small style="color:white !important;">
-                    {{ selectedTrain.fares[0].classType }}
-                  </v-btn>
+                    {{ selectedTrain.trainCode }} )
+                    <v-btn
+                      rounded
+                      color="info"
+                      small
+                      style="color: white !important"
+                    >
+                      {{ selectedTrain.fares[0].classType }}
+                    </v-btn>
                     <br />
                     <span
                       >Traveling from {{ selectedTrain.fromStationCity }} to
@@ -26,84 +32,98 @@
               </div>
 
               <div class="col-md-7">
-                  <div class="row">
-                         <div class="col-md-3">
-                <div class="kh-rw">
-                  <p>
-                    {{ selectedTrain.fromStationCode }}
-                    <span>{{date_formated(selectedTrain.arrivalTime)}}</span>
-                  </p>
-                </div>
-              </div>
-
-              <div class="col-md-3">
-                <div class="kh-rw">
-                  <p>
-                    {{ selectedTrain.toStationCode }}
-                    <span>{{ date_formated(selectedTrain.departureTime) }}</span>
-                  </p>
-                </div>
-              </div>
-
-              <div class="col-md-3">
-                <div class="kh-rw">
-                  <p>
-                    Duration <span>{{ get_duration(selectedTrain.duration) }}</span>
-                  </p>
-                </div>
-              </div>
-
-              <div class="col-md-3">
-                <div class="kh-rw st">
-                  <p>
-                    Stops <span>{{ selectedTrain.stops }}</span>
-                  </p>
-                </div>
-              </div>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="kh-rw">
+                      <p>
+                        {{ selectedTrain.fromStationCode }}
+                        <span>{{
+                          date_formated(selectedTrain.arrivalTime)
+                        }}</span>
+                      </p>
+                    </div>
                   </div>
+
+                  <div class="col-md-3">
+                    <div class="kh-rw">
+                      <p>
+                        {{ selectedTrain.toStationCode }}
+                        <span>{{
+                          date_formated(selectedTrain.departureTime)
+                        }}</span>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-3">
+                    <div class="kh-rw">
+                      <p>
+                        Duration
+                        <span>{{ get_duration(selectedTrain.duration) }}</span>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-3">
+                    <div class="kh-rw st">
+                      <p>
+                        Stops <span>{{ selectedTrain.stops }}</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-
           </div>
         </div>
 
-         <div class="col-md-12"> 
-                      <div class="row mt-1">
-               
-<div class="coach" v-for="trainCoach in trainCoachDTO" :key="trainCoach" @click="selectCoach($event,trainCoach)" >
-    Coach# {{trainCoach.coachNo}} <br>
-    Vacant: {{ trainCoach.vacantSeats }} 
-  </div>
+        <div class="col-md-12">
+          <div class="row mt-1">
+            <div
+              class="coach" id="coach-div"
+              v-for="trainCoach in trainCoachDTO"
+              :key="trainCoach"
+              @click="selectCoach($event, trainCoach)"
+            >
+              Coach# {{ trainCoach.coachNo }} <br />
+              Vacant: {{ trainCoach.vacantSeats }}
             </div>
+          </div>
         </div>
-          
-          <div class="container" >
-            <h4>Seats</h4>
-            <hr>
-           <div class="row mt-1">
 
-            
-  <div class="seat" v-for="seat in parseInt(seatsCount)" :key="seat" @click="select($event)"  :class="{reserved:IsReserved(berth+'S')}">
-    {{seat}}S
-  </div>
-
-
-
+        <div class="container">
+          <h4>Seats</h4>
+          <hr />
+          <div class="row mt-1">
+            <div
+              class="seat"
+              v-for="seat in parseInt(seatsCount)"
+              :key="seat"
+              @click="select($event)"
+              :class="{ reserved: IsReserved(seat + 'S') }"
+            >
+              {{ seat }}S
             </div>
+          </div>
+        </div>
+        <div class="container">
+          <h4>Berth</h4>
+          <hr />
+          <div class="row mt-1">
+            <div
+              class="seat"
+              v-for="berth in parseInt(berthCount)"
+              :key="berth"
+              @click="select($event)"
+              :class="{ reserved: IsReserved(berth + 'B') }"
+            >
+              {{ berth }}B
             </div>
-             <div class="container" >
-            <h4>Berth</h4>
-            <hr>
-            <div class="row mt-1">
-               
-<div class="seat" v-for="berth in parseInt(berthCount)" :key="berth" @click="select($event)" :class="{reserved:IsReserved(berth+'B')}" >
-    {{berth}}B
-  </div>
-            </div>
-           </div>
-           </div>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
 </template>
 <script>
 import bookingService from "@/services/booking";
@@ -153,30 +173,32 @@ export default {
     get_duration(date) {
       let time = moment(date).subtract(1, "days").format("h:mm a");
       let res = moment.duration(time, "hours").hours();
-      console.log(res);
     },
-    select(event){
-    if (event.currentTarget.className == "seat" && event.currentTarget.className != "seat reserved") {
+    select(event) {
+      if (
+        event.currentTarget.className == "seat" &&
+        event.currentTarget.className != "seat reserved"
+      ) {
         event.currentTarget.className = "booked";
       } else {
-        if(event.currentTarget.className == "booked"){
+        if (event.currentTarget.className == "booked") {
           event.currentTarget.className = "seat";
         }
         // event.currentTarget.className = "seat";
       }
     },
-    IsReserved(num){
-   if (this.trainCoachBookedDTOs.find(x => x.seatNo === num)) {
-      return true;
-    } else {
-      return false;
-    }
+    IsReserved(num) {
+      if (this.trainCoachBookedDTOs.find((x) => x.seatNo === num)) {
+        return true;
+      } else {
+        return false;
+      }
     },
     selectCoach(event, coach) {
-      if (event.currentTarget.className == "coach") {
+      if (event.target.className == "coach") {
         event.currentTarget.className = "selected";
       } else {
-        event.currentTarget.className = "coach";
+        event.target.className = "coach";
       }
       this.trainCoachBookedDTOs = coach.trainCoachBookedDTOs;
       this.berthCount = coach.berthCount;
